c5820c582495c7a4a01382b9b40268ecf2ed035c
dianka759 <dianka759@gmail.com>
2022-01-11 15:48:22 -0500

    added alt solution

--- React/Intro_to_React/prop-it-up/src/components/PersonCard.js
+++ React/Intro_to_React/prop-it-up/src/components/PersonCard.js
@@ -14,3 +14,27 @@ class PersonCard extends Component {
 }
 
 export default PersonCard;
+
+
+
+// Another Solution //
+
+// import React, { Component } from 'react';
+// import classes from './MyNewComponent.module.css';
+
+// class PersonComponent extends Component{
+//     render(){
+//         return(
+//             <div className={classes.card}>
+//                 <div className={classes.content}>
+//                     <h1>{this.props.lastName}, {this.props.firstName}</h1>
+//                     <p>Age: {this.props.age}</p>
+//                     <p>Hair Color: {this.props.hairColor}</p>
+//                 </div>
+//             </div>
+//         );
+
+//     }
+// }
+
+// export default PersonComponent;
