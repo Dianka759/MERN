553918faddd9a2108fde316813db609e18df4f51
dianka759 <dianka759@gmail.com>
2022-01-12 14:52:26 -0500

    first commit

--- React/Intro_to_React/class-demo/src/App.js
+++ React/Intro_to_React/class-demo/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';4
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Box from './components/Box';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>HIELLO</h1> 
+      <Box text="Heyyyya ittaa meeeeya" dark={true} />
+      <Box text="Heyaa itttaa youuuuaaaa" dark={false} />
+      <Box text="yes."  dark={true} />
+      <Box text="A POTATO FLEW AROUND ZE ROOM"dark={false} />
+      <Box text="hermurgur" dark={true}>
+        <h1> WHAT IS THIS </h1>
+      </Box>
     </div>
   );
 }
---
+++ React/Intro_to_React/class-demo/src/components/Box.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Box extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={this.props.dark ? "dark" : "light"}>
+                <marquee>
+                    {this.props.children}
+                    <h1>
+                        {/* {this.props.dark ? <h1>DARK MODE ENGAGED‽‽</h1> : <span></span>} */}
+                        {this.props.dark && <h1>DARK MODE ENGAGED‽‽‽‽‽</h1>}
+                        {this.props.text}
+                    </h1>
+                </marquee>
+            </div>
+        )
+    }
+}
+
+export default Box;
