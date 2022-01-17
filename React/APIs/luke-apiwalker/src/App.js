881f043606a28ffe3c3072e4a1af955be2173ff2
dianka759 <dianka759@gmail.com>
2022-01-16 20:38:08 -0500

    first commit

---
+++ React/APIs/luke-apiwalker/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Form from './components/Form';
+import Results from './components/Results';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Form} />
+          <Route path='/:category/:id'>
+            <Form></Form>
+            <Results></Results>
+          </Route>/
+          <Route path="/"
+            render={() => (<div className='mt-5 fs-3 fw-bold text-danger'> Invalid URL.</div>)}
+          />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
